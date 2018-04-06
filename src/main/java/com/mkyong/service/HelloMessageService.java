9604135ce3e1c9bbd9892a9a8988861414474674
Shubham <das.shubham011@gmail.com>
2018-04-06 18:37:51 +0530

    changing property value

--- src/main/java/com/mkyong/service/HelloMessageService.java
+++ src/main/java/com/mkyong/service/HelloMessageService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloMessageService {
 
-    @Value("${name:unknown}")
+    @Value("${name:Jiya}")
     private String name;
 
     public String getMessage() {
